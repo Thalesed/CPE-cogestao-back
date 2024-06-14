f1c26005029c29b3b6f76f13d7d6ce1abe88839e
Thalesed <thalesdeduardos@gmail.com>
2024-06-14 16:48:52 -0300

    configurações iniciais

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const app  = require("./App");
+
+const PORT =  8000;
+
+app.listen(PORT, () => console.log("Servidor Rodando"))
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+
+
+const rotas = Router();
+
+
+
+module.exports = rotas;
