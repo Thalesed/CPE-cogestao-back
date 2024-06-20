fbda329f89cd09ee563e73cffe316a452f3fa440
Thales <thalesdeduardos@gmail.com>
2024-06-20 19:53:21 -0300

    back

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,19 @@ const { Router } = require("express");
 const eventsValidator = require("./Validators/eventsValidator");
 const eventsController = require("./Controllers/eventsController");
 
+const JWTVerify = require("./Middlewares/JWTVerify");
+
 const rotas = Router();
 
 
-rotas.post("/eventos", eventsValidator.create, eventsController.create);
+rotas.post("/eventos", JWTVerify, eventsValidator.create, eventsController.create);
 
-rotas.get("/eventos", eventsController.read);
+rotas.get("/eventos", JWTVerify, eventsController.read);
 
-rotas.delete("/eventos/:id_usuario", eventsValidator.destroy, eventsController.delete);
+rotas.delete("/eventos/:id_usuario", JWTVerify, eventsValidator.destroy, eventsController.delete);
 
 rotas.put(
-  "/eventos/:id", eventsValidator.update, eventsController.update);
+  "/eventos/:id", JWTVerify, eventsValidator.update, eventsController.update);
 
 
 
